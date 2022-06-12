2cb15afe3cc6bb33b089052192add7f4089fd548
Braian Camilo <braianc.piedrahitar@uqvirtual.edu.co>
2022-06-11 20:07:58 -0500

    Proyecto configurado para el despliegue

--- src/keys.js
+++ src/keys.js
@@ -1,14 +1,16 @@
 module.exports = {
     database: {
 
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'test_analisis'
-
-        //host: 'testanalisis.mysql.database.azure.com',
-        //user: 'administrador',
-        //password: 'test1234!',
+        //Base de datos local
+        //host: 'localhost',
+        //user: 'root',
+        //password: 'root',
         //database: 'test_analisis'
+
+        //Base de datos de la nube (produccion)
+        host: 'testanalisis.mysql.database.azure.com',
+        user: 'administrador',
+        password: 'test1234!',
+        database: 'test_analisis'
     }
 }
