50b0949a0c6e9a37c766bedf159e879def0c8c6b
Sebastian Quintero <sebastian.quinteroo@uqvirtual.edu.co>
2022-06-12 00:43:27 -0500

    Cambio de base

--- src/keys.js
+++ src/keys.js
@@ -2,15 +2,15 @@ module.exports = {
     database: {
 
         //Base de datos local
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'test_analisis'
+        //host: 'localhost',
+        //user: 'root',
+        //password: 'root',
+        //database: 'test_analisis'
 
         //Base de datos de la nube (produccion)
-        //host: 'testanalisis.mysql.database.azure.com',
-        //user: 'administrador',
-        //password: 'test1234!',
-        //database: 'test_analisis'
+        host: 'testanalisis.mysql.database.azure.com',
+        user: 'administrador',
+        password: 'test1234!',
+        database: 'test_analisis'
     }
 }
